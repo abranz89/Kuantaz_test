a66f4f88e5dce1a025214a5fb903f42365aab573
Abraham Contreras <abran_contreras@hotmail.com>
2024-04-28 12:48:32 -0400

    Initialization of project

---
+++ app/Http/Controllers/Api/KuantazController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KuantazController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+    }
+
+}
